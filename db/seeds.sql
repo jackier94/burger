989f8ead3f6e1f536ea9c3cf72cd8e1bc9987ae2
Jaquelin Rocha <jaquir16@gmail.com>
2020-08-10 19:41:11 -0700

    set folder order, MVC

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Triple Meat');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Hawaiian');
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Sylvester Balone', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Hammin', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Avocado Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Parmesan Burger', true );
